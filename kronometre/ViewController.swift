c8a2ffd195b4a88cdc2c89b833ccb91bebcf38d5
omerUnver <muhammedomerunver@gmail.com>
2022-04-15 20:58:01 +0300

    Kronometre

--- kronometre/ViewController.swift
+++ kronometre/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var zaman = 0
+    var zamanlayici = Timer()
+    @IBOutlet weak var saat: UILabel!
+    
+    @IBAction func basla(_ sender: Any) {
+        saat.text = String(zaman)
+        zaman = 0
+        zamanlayici =  Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerApp), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        zamanlayici.invalidate()
+    }
+    
+    @objc func timerApp(){
+        saat.text = String(zaman)
+        zaman += 1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
